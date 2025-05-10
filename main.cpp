002963f17f31ce95ee92447c2f38e169f8a9144f
Bowie <tauane581730@gmail.com>
2025-05-10 18:23:39 -0300

    fix: tempo de execucão para micro segundos
    fix: tempo de execucão para micro segundos e alteraão dos nomes com acentos.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <locale>
 #include "util.h"
 #include "bucket_sort.h"
 #include "quick_sort.h"
@@ -8,34 +9,34 @@ using namespace std::chrono;
 
 int main() {
     string caminho = "alunos.csv";
-
+    
     // Lê os dados do arquivo CSV
     vector<Aluno> alunos = lerCSV(caminho);
 
     cout << "Alunos lidos:\n";
     imprimirAlunos(alunos);
 
-    // ---------- BUCKET SORT ----------
+    // ---------- BUCKET SORT ----------    
     auto inicioBS = high_resolution_clock::now();
     vector<Aluno> alunosBucket = alunos; // cópia para Bucket Sort
     bucketSort(alunosBucket);
     auto fimBS = high_resolution_clock::now();
-    auto duracaoBS = duration_cast<milliseconds>(fimBS - inicioBS);
+    auto duracaoBS = duration_cast<microseconds>(fimBS - inicioBS);  // Medição em microsegundos
 
     cout << "\nAlunos ordenados (Bucket Sort):\n";
     imprimirAlunos(alunosBucket);
-    cout << "\nTempo de execucao (Bucket Sort): " << duracaoBS.count() << " ms\n";
+    cout << "\nTempo de execucao (Bucket Sort): " << duracaoBS.count() << " microssegundos\n";
 
     // ---------- QUICK SORT ----------
     auto inicioQS = high_resolution_clock::now();
     vector<Aluno> alunosQuick = alunos; // cópia para Quick Sort
     quickSort(alunosQuick, 0, alunosQuick.size() - 1);
     auto fimQS = high_resolution_clock::now();
-    auto duracaoQS = duration_cast<milliseconds>(fimQS - inicioQS);
+    auto duracaoQS = duration_cast<microseconds>(fimQS - inicioQS);  // Medição em microsegundos
 
     cout << "\nAlunos ordenados (Quick Sort):\n";
     imprimirAlunos(alunosQuick);
-    cout << "\nTempo de execucao (Quick Sort): " << duracaoQS.count() << " ms\n";
+    cout << "\nTempo de execucao (Quick Sort): " << duracaoQS.count() << " microssegundos\n";
 
     return 0;
 }
